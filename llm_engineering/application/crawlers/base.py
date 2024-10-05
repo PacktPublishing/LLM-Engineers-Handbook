d9a740006933b2171e79125aeb9bdfd03abaf7b6
iusztinpaul <p.e.iusztin@gmail.com>
2024-10-05 18:25:36 +0300

    feat: Fix selenium crawler

--- llm_engineering/application/crawlers/base.py
+++ llm_engineering/application/crawlers/base.py
@@ -27,22 +27,17 @@ class BaseSeleniumCrawler(BaseCrawler, ABC):
 
         options.add_argument("--no-sandbox")
         options.add_argument("--headless=new")
-        options.add_argument("--single-process")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
         options.add_argument("--log-level=3")
         options.add_argument("--disable-popup-blocking")
         options.add_argument("--disable-notifications")
-        options.add_argument("--disable-dev-tools")
         options.add_argument("--disable-extensions")
-        options.add_argument("--disable-software-rasterizer")
         options.add_argument("--disable-background-networking")
         options.add_argument("--ignore-certificate-errors")
-        options.add_argument("--no-zygote")
         options.add_argument(f"--user-data-dir={mkdtemp()}")
         options.add_argument(f"--data-path={mkdtemp()}")
         options.add_argument(f"--disk-cache-dir={mkdtemp()}")
-        options.add_argument("--remote-debugging-port=9225")
+        options.add_argument("--remote-debugging-port=9226")
 
         self.set_extra_driver_options(options)
 
